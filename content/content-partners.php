e506db6efeaf842a9f36613b57c1e3fbffbdc293
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-10 15:49:45 -0300

    Some adjusts on partners slide

---
+++ content/content-partners.php
@@ -0,0 +1,52 @@
+<?php
+// Criando uma nova consulta para recuperar os posts do tipo "fornecedor"
+$args = array(
+    'post_type' => 'fornecedor',
+    'posts_per_page' => 4 // Número de posts para exibir
+);
+$custom_query = new WP_Query($args);
+
+// Verificar se existem posts
+if ($custom_query->have_posts()) {?>
+    <!-- grid -->
+    <section>
+        <div class="wrapper-grid category-slider">
+            <div class="swiper-wrapper">
+    <?php
+    while ($custom_query->have_posts()) {
+        $custom_query->the_post();
+        $categories = get_the_terms(get_the_ID(), 'tipo');
+        if ($categories && !is_wp_error($categories)):
+            foreach($categories as $category):
+                $category_link = get_term_link($category)?>
+                <div class="swiper-slide">
+                    <div class="wrapper-grid__controller">
+                        <div class="wrapper-grid__cta" style="background: no-repeat center center url(<?php echo get_bloginfo('template_url')?>/src/images/casamento.jpg);">
+                            <a href="<?php echo $category_link;?>" alt="<?php echo $category->name;?>">
+                                <?php
+                                    echo $category->name;
+                                ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+        <?php 
+            endforeach;
+        endif;
+        }?>
+            </div>
+        </div>
+    </section>
+<?php
+    // Restaurar os dados originais do post
+    wp_reset_postdata();
+} else {
+    // Caso não haja posts do tipo "fornecedor" para exibir
+    echo 'Nenhum fornecedor encontrado.';
+}
+
+?>
+
+       
+            
+    
