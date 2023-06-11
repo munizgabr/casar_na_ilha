bf7fa7f98f8a30dbbcbb363a8842fc3aeafadefa
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-11 14:08:20 -0300

    remove cached files

--- build/js/components/mask.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function() {
-    $("input[type=tel]").mask("(99) 99999-9999");
-    $("input[name='telefone']").mask("(99) 99999-9999");
-    $("input[name='celular']").mask("(99) 99999-9999");
-}, 3000);
