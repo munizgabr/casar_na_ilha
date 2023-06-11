382cefad3eb087deeb711fa8481dcb55d2a00375
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-11 11:39:29 -0300

    Finish content partners

--- content/content-partners.php
+++ content/content-partners.php
@@ -12,16 +12,15 @@ if ($custom_query->have_posts()) {?>
     <section>
         <div class="wrapper-grid category-slider">
             <div class="swiper-wrapper">
-    <?php
-    while ($custom_query->have_posts()) {
-        $custom_query->the_post();
-        $categories = get_the_terms(get_the_ID(), 'tipo');
-        if ($categories && !is_wp_error($categories)):
-            foreach($categories as $category):
-                $category_link = get_term_link($category)?>
+            <?php
+            $categories = get_terms('tipo'); // Obtenha todas as categorias do tipo 'tipo'
+
+            foreach ($categories as $category):
+                $image = get_field('category_image', $category);?>
                 <div class="swiper-slide">
+                    <?php $category_link = get_term_link($category);?>
                     <div class="wrapper-grid__controller">
-                        <div class="wrapper-grid__cta" style="background: no-repeat center center url(<?php echo get_bloginfo('template_url')?>/src/images/casamento.jpg);">
+                    <div class="wrapper-grid__cta" style="background: no-repeat center center url(<?php echo !empty($image) ? $image : get_bloginfo('template_url') . '/src/images/casamento.jpg'; ?>);">
                             <a href="<?php echo $category_link;?>" alt="<?php echo $category->name;?>">
                                 <?php
                                     echo $category->name;
@@ -30,11 +29,9 @@ if ($custom_query->have_posts()) {?>
                         </div>
                     </div>
                 </div>
-        <?php 
-            endforeach;
-        endif;
-        }?>
+            <?php endforeach;?>
             </div>
+            <div class="swiper-pagination"></div>
         </div>
     </section>
 <?php
@@ -44,7 +41,6 @@ if ($custom_query->have_posts()) {?>
     // Caso não haja posts do tipo "fornecedor" para exibir
     echo 'Nenhum fornecedor encontrado.';
 }
-
 ?>
 
        
