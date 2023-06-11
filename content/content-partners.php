522a524717ec3645a839145dd0deed37633e890c
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-11 14:01:04 -0300

    finish content partners responsive

--- content/content-partners.php
+++ content/content-partners.php
@@ -2,36 +2,33 @@
 // Criando uma nova consulta para recuperar os posts do tipo "fornecedor"
 $args = array(
     'post_type' => 'fornecedor',
-    'posts_per_page' => 4 // Número de posts para exibir
+    'posts_per_page' => 1 // Número de posts para exibir
 );
 $custom_query = new WP_Query($args);
 
 // Verificar se existem posts
 if ($custom_query->have_posts()) {?>
     <!-- grid -->
-    <section>
-        <div class="wrapper-grid category-slider">
-            <div class="swiper-wrapper">
-            <?php
-            $categories = get_terms('tipo'); // Obtenha todas as categorias do tipo 'tipo'
+    <section class="wrapper-grid category-slider">
+        <div class="swiper-wrapper">
+        <?php
+        $categories = get_terms('tipo'); // Obtenha todas as categorias do tipo 'tipo'
 
-            foreach ($categories as $category):
-                $image = get_field('category_image', $category);?>
-                <div class="swiper-slide">
-                    <?php $category_link = get_term_link($category);?>
-                    <div class="wrapper-grid__controller">
+        foreach ($categories as $category):
+            $image = get_field('category_image', $category);?>
+            <div class="swiper-slide">
+                <?php $category_link = get_term_link($category);?>
+                <div class="wrapper-grid__controller">
                     <div class="wrapper-grid__cta" style="background: no-repeat center center url(<?php echo !empty($image) ? $image : get_bloginfo('template_url') . '/src/images/casamento.jpg'; ?>);">
-                            <a href="<?php echo $category_link;?>" alt="<?php echo $category->name;?>">
-                                <?php
-                                    echo $category->name;
-                                ?>
-                            </a>
-                        </div>
+                        <a href="<?php echo $category_link;?>" alt="<?php echo $category->name;?>">
+                            <?php
+                                echo $category->name;
+                            ?>
+                        </a>
                     </div>
                 </div>
-            <?php endforeach;?>
             </div>
-            <div class="swiper-pagination"></div>
+        <?php endforeach;?>
         </div>
     </section>
 <?php
--- functions/widgets.php
+++ functions/widgets.php
@@ -1,22 +1,22 @@
 <?php
 
-add_action( 'widgets_init', 'somadev_widgets_init' );
+add_action( 'widgets_init', 'bytebloom_widgets_init' );
 
-function somadev_widgets_init() {
+function bytebloom_widgets_init() {
 
     register_sidebar ( array (
-        'name' => __( 'Topo Esquerda', 'somadev' ),
+        'name' => __( 'Topo Esquerda', 'bytebloom' ),
         'id' => 'topoesquerda',
-        'description' => __( 'Exibição de conteúdo no topo a esquerda', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo no topo a esquerda', 'bytebloom' ),
         'before_widget' => '',
         'after_widget' => '',
         'before_title' => '',
         'after_title' => '',
     ));
     register_sidebar ( array (
-        'name' => __( 'Sidebar', 'somadev' ),
+        'name' => __( 'Sidebar', 'bytebloom' ),
         'id' => 'sidebar',
-        'description' => __( 'Exibição de conteúdo na sidebar', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo na sidebar', 'bytebloom' ),
         'before_widget' => '',
         'after_widget' => '',
         'before_title' => '<h2 class="widget-title">',
@@ -24,9 +24,9 @@ function somadev_widgets_init() {
     ));
 
     register_sidebar ( array (
-        'name' => __( 'Topo Direita', 'somadev' ),
+        'name' => __( 'Topo Direita', 'bytebloom' ),
         'id' => 'topodireita',
-        'description' => __( 'Exibição de conteúdo no topo a direita', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo no topo a direita', 'bytebloom' ),
         'before_widget' => '',
         'after_widget' => '',
         'before_title' => '',
@@ -34,9 +34,9 @@ function somadev_widgets_init() {
     ));
 
     register_sidebar ( array (
-        'name' => __( 'Footer 1', 'somadev' ),
+        'name' => __( 'Footer 1', 'bytebloom' ),
         'id' => 'footer1',
-        'description' => __( 'Exibição de conteúdo na coluna 1 do rodapé', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo na coluna 1 do rodapé', 'bytebloom' ),
         'before_widget' => '<div class="widget">',
         'after_widget' => '</div>',
         'before_title' => '<h4>',
@@ -44,9 +44,9 @@ function somadev_widgets_init() {
     ));
 
     register_sidebar ( array (
-        'name' => __( 'Footer 2', 'somadev' ),
+        'name' => __( 'Footer 2', 'bytebloom' ),
         'id' => 'footer2',
-        'description' => __( 'Exibição de conteúdo na coluna 2 do rodapé', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo na coluna 2 do rodapé', 'bytebloom' ),
         'before_widget' => '<div class="widget">',
         'after_widget' => '</div>',
         'before_title' => '<h4>',
@@ -54,9 +54,9 @@ function somadev_widgets_init() {
     ));
 
     register_sidebar ( array (
-        'name' => __( 'Footer 3', 'somadev' ),
+        'name' => __( 'Footer 3', 'bytebloom' ),
         'id' => 'footer3',
-        'description' => __( 'Exibição de conteúdo na coluna 3 do rodapé', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo na coluna 3 do rodapé', 'bytebloom' ),
         'before_widget' => '<div class="widget">',
         'after_widget' => '</div>',
         'before_title' => '<h4>',
@@ -64,9 +64,9 @@ function somadev_widgets_init() {
     ));
 
     register_sidebar ( array (
-        'name' => __( 'Footer 4', 'somadev' ),
+        'name' => __( 'Footer 4', 'bytebloom' ),
         'id' => 'footer4',
-        'description' => __( 'Exibição de conteúdo na coluna 4 do rodapé', 'somadev' ),
+        'description' => __( 'Exibição de conteúdo na coluna 4 do rodapé', 'bytebloom' ),
         'before_widget' => '<div class="widget">',
         'after_widget' => '</div>',
         'before_title' => '<h4>',
@@ -74,9 +74,9 @@ function somadev_widgets_init() {
     ));
 
     register_sidebar ( array (
-        'name' => __( 'Search bar', 'somadev' ),
+        'name' => __( 'Search bar', 'bytebloom' ),
         'id' => 'search',
-        'description' => __( 'Exibição de barra de pesquisa na home', 'somadev' ),
+        'description' => __( 'Exibição de barra de pesquisa na home', 'bytebloom' ),
         'before_widget' => '<div class="widget searchbar">',
         'after_widget' => '</div>',
         'before_title' => '<h4>',
