5ff948f67a8b0acd26f82f85b3730f399326b577
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-19 15:01:38 -0300

    finish banner award

--- page-teste.php
+++ page-teste.php
@@ -22,7 +22,7 @@ get_header();?>
   </div>
 </section> -->
 
-
+<!-- Grid category -->
 <section class="fornecedores grid gap-4 grid-cols-3 p-4">
     <div class='rounded-md shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] hover:scale-[1.025] hover:ease-in hover:duration-300  flex gap-5 flex-col items-center p-6'>
         <img src="<?php bloginfo('template_url');?>/src/images/noiva.png" alt="Categoria vestido de noiva">
@@ -48,6 +48,18 @@ get_header();?>
         <img src="<?php bloginfo('template_url');?>/src/images/noiva.png" alt="Categoria vestido de noiva">
         <span class='text-black font-bold text-sm'>Vestidos de noiva</span>
     </div>
+</section>
+
+<!-- fornecedores premium -->
+<section class="grid grid-cols-1 gap-4 p-4">
+    <div class="flex space-x-4">
+        <div class="flex flex-col place-content-between">
+            <h3 class="text-xl font-bold">Fornecedores PREMIUM</h3>
+            <span class="text-lg">Consulte os fornecedores mais procurados da região</span>
+        </div>
+        <img src="<?php bloginfo('template_url');?>/src/images/fornecedor-premium.png" alt="Selo de fornecedor Premium">
+    </div>
+    <div></div>
 </section>
 
 <?php get_footer(); ?>
