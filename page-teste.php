3ce478444d571e55b2021a12a0c1c3150db82988
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-22 19:07:54 -0300

    Make premium section

--- page-teste.php
+++ page-teste.php
@@ -19,14 +19,20 @@ get_header();?>
 </section>
 
 <!-- Highlights -->
-<section class="pb-4 px-4 grid grid-cols-2">
-  <div class="shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg overflow-hidden">
-    <img src="<?php bloginfo('template_url');?>/src/images/carro-antigo.jpeg" alt="Carro">
-    <div class="p-4">
-      <span class="text-lg text-black font-bold">Empresa</span>
-      <div>4* Bairro, Cidade</div>
-      <div class="grid grid-cols-[20px_auto] gap-2">
-        <img src="<?php bloginfo('template_url');?>/src/images/coin.png" alt="Custo"> A partir de R$1000,00
+<section class="grid grid-cols-1 items-center">
+  <div class="flex flex-row place-content-between px-4 items-center">
+    <h2 class="font-bold text0black text-xl">Empresas em destaque</h2>
+    <img src="<?php bloginfo('template_url');?>/src/images/fornecedor-premium.png" alt="Fornecedores premium">
+  </div>
+  <div class="pb-4 px-4 grid grid-cols-2">
+    <div class="shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg overflow-hidden">
+      <img src="<?php bloginfo('template_url');?>/src/images/carro-antigo.jpeg" alt="Carro">
+      <div class="p-4">
+        <span class="text-lg text-black font-bold">Empresa</span>
+        <div>4* Bairro, Cidade</div>
+        <div class="grid grid-cols-[20px_auto] gap-2">
+          <img src="<?php bloginfo('template_url');?>/src/images/coin.png" alt="Custo"> A partir de R$1000,00
+        </div>
       </div>
     </div>
   </div>
