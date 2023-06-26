4f7cf9be5f9f7bd1a73d717d5d25a9d23d2e8d16
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-06-26 15:02:23 -0300

    Create section blog posts

--- page-teste.php
+++ page-teste.php
@@ -82,40 +82,60 @@ get_header();
     </div>
   </div>
 </section>
+
 <!-- Blog posts -->
 <section class="p-4 bg-gradient-to-b from-[#176B87] to-[#001C30]">
   <span class="uppercase text-lg text-center text-white my-4 font-bold block">Últimas notícias</span>
-  <div class="center p-4 grid grid-cols-3 grid-rows-3 border gap-3 border-black bg-white rounded-lg items-center justify-center text-center">
-    <a href="#" class="principal grid grid-cols-2  row-start-1 row-end-3 col-start-1 col-end-4 font-semibold text-black text-xl" title="teste">
-      <img src="<?php bloginfo('template_url');?>/src/images/ensaio.jpg" alt="teste">
-      <div class="w-full p-4 bg-[#ffffff69]">
-        <span class="font-bold text-black">titulo da materia</span>
-        <p class="opacity-[100%] text-black text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ipsum ad earum vel. Impedit maiores, odio voluptatibus quis autem tempora quisquam ducimus, libero atque laboriosam au.</p>
-      </div>
-    </a>
-    <a href="" class="secundario font-semibold text-black text-lg relative">
-      <img src="<?php bloginfo('template_url');?>/src/images/ensaio.jpg" alt="ensaio">
-      <div class="absolute bottom-0 w-full bg-white opacity-75">
-        <span>Título</span>
-        <p class="opacity-[100%]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ratione cum doloremque fugit.</p>
-      </div>
-    </a>
-    <a href="" class="secundario font-semibold text-black text-lg relative">
-      <img src="<?php bloginfo('template_url');?>/src/images/ensaio.jpg" alt="ensaio">
-      <div class="absolute bottom-0 w-full bg-white opacity-75">
-        <span>Título</span>
-        <p class="opacity-[100%]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ratione cum doloremque fugit.</p>
-      </div>
-    </a>
-    <a href="" class="secundario font-semibold text-black text-lg relative">
-      <img src="<?php bloginfo('template_url');?>/src/images/ensaio.jpg" alt="ensaio">
-      <div class="absolute bottom-0 w-full bg-white opacity-75">
-        <span>Título</span>
-        <p class="opacity-[100%]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ratione cum doloremque fugit.</p>
-      </div>
-    </a>
+  <div class="center p-4 grid grid-cols-3 grid-rows-3 gap-3 bg-[#ffffff66] rounded-lg items-center justify-center text-center">
+    <?php
+    $recent_posts = get_posts(array(
+      'post_type' => 'post',
+      'numberposts' => 4,
+      'order' => 'DESC' // Ordenar em ordem decrescente
+    ));
+    $post_count = count($recent_posts);
+
+    // Verificar se há pelo menos um post
+    if ($post_count > 0) {
+      // Último post
+      $last_post = $recent_posts[0];
+      setup_postdata($last_post);
+      $last_post_thumbnail = get_the_post_thumbnail_url($last_post);
+      $last_post_title = get_the_title($last_post);
+      $last_post_content = wp_trim_words(get_the_content($last_post), 50, '...');
+    ?>
+      <a href="<?php echo get_permalink($last_post); ?>" class="principal h-full grid grid-cols-2 row-start-1 row-end-3 col-start-1 col-end-4" title="<?php echo $last_post_title; ?>">
+        <img class="h-full object-cover" src="<?php echo (!empty($last_post_thumbnail)) ? $last_post_thumbnail : bloginfo('template_url') . '/src/images/ensaio.jpg'; ?>" alt="<?php echo $last_post_title; ?>">
+        <div class="w-full p-4 bg-white">
+          <span class="font-bold text-lg text-black uppercase"><?php echo $last_post_title; ?></span>
+          <p class="opacity-[100%] text-black text-sm"><?php echo $last_post_content; ?></p>
+        </div>
+      </a>
+    <?php
+      // Demais posts
+      for ($i = 1; $i < $post_count; $i++) {
+        $post = $recent_posts[$i];
+        setup_postdata($post);
+        $post_thumbnail = get_the_post_thumbnail_url($post);
+        $post_title = get_the_title($post);
+        $post_content = wp_trim_words(get_the_content($post), 10, '...');
+    ?>
+        <a href="<?php echo get_permalink($post); ?>" class="secundario h-full font-semibold text-black text-lg relative">
+          <img class="w-full h-full object-cover" src="<?php echo (!empty($post_thumbnail)) ? $post_thumbnail : bloginfo('template_url') . '/src/images/ensaio.jpg'; ?>" alt="<?php echo $post_title; ?>">
+          <div class="absolute bottom-0 w-full bg-[#ffffff66]">
+            <span class="font-bold text-lg text-black uppercase"><?php echo $post_title; ?></span>
+            <p class="opacity-[100%] text-black text-sm"><?php echo $post_content; ?></p>
+          </div>
+        </a>
+    <?php
+      }
+    }
+    wp_reset_postdata();
+    ?>
   </div>
 </section>
+
+
 <!-- Categories -->
 <?php
 $terms = get_terms('tipo');
